c9e38b738f6cb977041622ff7e3fdd36620995f4
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-08-23 09:03:42 -0400

    inter pause

--- Intermediate.ps1
+++ Intermediate.ps1
@@ -1,3 +1,4 @@
 $driveletter = $pwd.drive.name #retourne la lettre du disque actuel
 $root = "$driveletter" + ":" #rajoute  : pour que sa fit dans le path
-Start-Process "$root\_Tech\menu.exe" 
+Start-Process "$root\_Tech\menu.exe"
+pause
