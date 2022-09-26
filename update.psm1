4e5c91ad1272569d4296fffe3eeb679d9abd347e
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-09-25 21:56:11 -0400

    version fonctionnel de updateapp

--- update.psm1
+++ update.psm1
@@ -4,7 +4,7 @@ $applications = "$root\\_Tech\applications" #chemin du dossier applications
 
 function Update($categorie,$nomApplicationAMettreAJour,$liengithub,$lienappligithub)
 {
-    $dossierTemp = "$root\_Tech\applications\$categorie\source\$nomApplicationAMettreAJour\Temp" #path
+    $dossierTemp = "$root\_Tech\applications\$categorie\source\$nomApplicationAMettreAJour\Temp" #path du dossier Temp
     try 
     {
         New-Item -Path $dossierTemp -ItemType Directory -Force | Out-Null #créer dossier temp
@@ -15,23 +15,23 @@ function Update($categorie,$nomApplicationAMettreAJour,$liengithub,$lienappligit
         return
     }
 
-    #Il faut download le fichier version depuis github
+    #Download le fichier version depuis github
     Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/versions/main/Diagnostique/speccy.version.txt' -OutFile "$applications\$categorie\source\$nomApplicationAMettreAJour\Temp\speccy.version.txt" | Out-Null
 
-    #Il faut aller chercher le chiffre dans les 2 fichiers
+    #cherche le chiffre dans les 2 fichiers
     $valuedownloadfile = Get-Content -Path "$dossierTemp\*.version.txt" #fichier version nouveau
     $valueactualfile = Get-Content -Path "$applications\$categorie\source\$nomApplicationAMettreAJour\*.version.txt" #fichier version actuel
     
-    #Il faut comparer les 2 valeurs
+    #compare les 2 valeurs
     if ($valuedownloadfile -gt $valueactualfile) 
     { 
         try 
         {
             Write-Host "Mise à jour en cours..."
             Invoke-WebRequest $lienappligithub -OutFile "$applications\$categorie\source\Speccy.zip" | Out-Null
-            Expand-Archive "$applications\$categorie\source\Speccy.zip" "$applications\$categorie\source" -Force
-            Remove-Item "$applications\$categorie\source\Speccy.zip" -Force
-            Copy-Item "$applications\$categorie\source\$nomApplicationAMettreAJour\Temp\speccy.version.txt" -Destination "$applications\$categorie\source\$nomApplicationAMettreAJour\speccy.version.txt" -Force
+            Expand-Archive "$applications\$categorie\source\Speccy.zip" "$applications\$categorie\source" -Force | Out-Null
+            Remove-Item "$applications\$categorie\source\Speccy.zip" -Force | Out-Null
+            Copy-Item "$applications\$categorie\source\$nomApplicationAMettreAJour\Temp\speccy.version.txt" -Destination "$applications\$categorie\source\$nomApplicationAMettreAJour\speccy.version.txt" -Force | Out-Null #Met le fichier version a jour.
         }
         catch 
         {
@@ -42,3 +42,7 @@ function Update($categorie,$nomApplicationAMettreAJour,$liengithub,$lienappligit
     Remove-Item $dossierTemp -Recurse -Force #Supprime le dossier temp
     return  
 }
+
+#exemple de call
+#Import-Module "E:\_Tech\Applications\Source\update.psm1"
+#Update "Diagnostique" "Speccy" 'https://raw.githubusercontent.com/jeremyrenaud42/versions/main/Diagnostique/speccy.version.txt' 'https://raw.githubusercontent.com/jeremyrenaud42/Diagnostique/main/Speccy.zip'
